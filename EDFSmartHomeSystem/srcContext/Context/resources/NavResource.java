29936369af4171b97313b6db68b556758991a421
J60277 <J60277@DSP0712560.atlas.edf.fr>
2016-08-11 11:26:43 +0200

    fix resources

---
+++ EDFSmartHomeSystem/srcContext/Context/resources/NavResource.java
@@ -0,0 +1,48 @@
+package Context.resources;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+
+import Context.server.TemplateEngine;
+
+@Path("/")
+public class NavResource {
+	@GET
+	@Produces("text/html")
+	public String getHome(@Context HttpServletRequest req) {
+		HttpSession session = req.getSession();
+		TemplateEngine.setSession(req.getSession());
+		
+		if(session.getAttribute("userid")!=null){
+			String userid = (String) session.getAttribute("userid");
+			System.out.println("Vous etes identifiee en tant que :"+userid);
+			return TemplateEngine.buildFromFile("home.html");
+		}else{
+			return TemplateEngine.buildFromFile("login.html");
+		}
+	}
+	
+	@GET
+	@Path("/{filename}")
+	public String getFile(@PathParam("filename") String filename) {
+		return TemplateEngine.getFile(filename);
+	}
+	
+	@GET
+	@Produces("text/html")
+	@Path("/form/login")
+	public String getLogin(@Context HttpServletRequest req) {
+		TemplateEngine.setSession(req.getSession());
+		return TemplateEngine.buildFromFile("login.html");
+	}
+	
+
+
+
+	
+}
---
+++ EDFSmartHomeSystem/srcContext/Context/resources/UserResource.java
@@ -0,0 +1,79 @@
+package Context.resources;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MultivaluedMap;
+
+import Context.server.Server;
+import Context.server.TemplateEngine;
+import Context.Model.User;
+import Context.Model.Handler.UserHandler;
+
+@Path("/user")
+public class UserResource {
+	@GET
+	@Path("/id/{userid}")
+	@Produces("text/xml")
+	public User getUserFromId(@PathParam("userid") String userid) {
+		User u = Server.uh.getUserFromId(userid);
+		return u;
+	}
+
+	@POST
+	@Path("/create")
+	@Produces("text/html")
+	@Consumes("application/x-www-form-urlencoded")
+	public String create(@Context HttpServletRequest req, MultivaluedMap<String, String> formParams){
+		TemplateEngine.setSession(req.getSession());
+		String login = formParams.getFirst("login");
+		String password = formParams.getFirst("password");
+		Server.uh.createUser(login, password);
+		return TemplateEngine.goHome();
+	}	
+	
+	@POST
+	@Path("/login")
+	@Produces("text/html")
+	@Consumes("application/x-www-form-urlencoded")
+	public String login(@Context HttpServletRequest req, @FormParam("login") String login) {
+		TemplateEngine.setSession(req.getSession());
+		if(Server.uh.getUserFromLogin(login)!=null){
+			HttpSession session = req.getSession();
+			session.setAttribute("userid", User.createUserId(login));
+			System.out.println("USER LOGIN : "+login);
+		}
+		return TemplateEngine.goHome();
+	}	
+	
+	@GET
+	@Path("/logout")
+	@Produces("text/html")
+	public String logout(@Context HttpServletRequest req) {
+		TemplateEngine.setSession(req.getSession());
+		HttpSession session = req.getSession();
+		String userid = (String) session.getAttribute("userid");
+		System.out.println("USER LOGOUT : "+userid);
+		session.removeAttribute("userid");
+		return TemplateEngine.redirect("/", 2, "Vous avez été déconnecté. Vous allez être redirigé dans 2 secondes.");
+	}	
+	
+	@GET
+	@Path("/all/")
+	@Produces("text/xml")
+	public UserHandler getAllUsers() {
+		return Server.uh;
+	}
+
+	
+
+}
