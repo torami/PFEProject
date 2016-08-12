c421703f3d7a85a85e174030f52dfe83e8a66007
J60277 <J60277@DSP0712560.atlas.edf.fr>
2016-08-12 09:57:43 +0200

    create resource service

--- EDFSmartHomeSystem/srcService/service/resources/ServiceResource.java
+++ EDFSmartHomeSystem/srcService/service/resources/ServiceResource.java
@@ -1,21 +1,17 @@
 package service.resources;
 
-import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MultivaluedMap;
-
 import service.model.Service;
+import service.model.handlers.ServiceHandler;
 import service.server.Server;
+import service.server.TemplateEngine;
 
 
 @Path("/service")
@@ -28,39 +24,15 @@ public class ServiceResource {
 		return u;
 	}
 
-	@POST
-	@Path("/create")
-	@Produces("text/html")
-	@Consumes("application/x-www-form-urlencoded")
-	public String create(@Context HttpServletRequest req){
-		TemplateEngine.setSession(req.getSession());
-		String login = formParams.getFirst("login");
-		String password = formParams.getFirst("password");
-		Server.uh.createUser(login, password);
-		return TemplateEngine.goHome();
-	}	
-	
-	@POST
-	@Path("/login")
-	@Produces("text/html")
-	@Consumes("application/x-www-form-urlencoded")
-	public String login(@Context HttpServletRequest req, @FormParam("login") String login, MultivaluedMap<String, String> formParams) {
-		TemplateEngine.setSession(req.getSession());
-		String password = formParams.getFirst("password");
-		if(Server.uh.VerifyloginAndPassword(login, password)==true){
-			HttpSession session = req.getSession();
-			session.setAttribute("userid", User.createUserId(login));
-			System.out.println("USER LOGIN : "+login);
-		}
-		return TemplateEngine.goHome();
-	}	
+
+
 	
 	@GET
 	@Path("/logout")
 	@Produces("text/html")
 	public String logout(@Context HttpServletRequest req) {
 		TemplateEngine.setSession(req.getSession());
-		HttpSession session = req.get	 Session();
+		HttpSession session = req.getSession();
 		String userid = (String) session.getAttribute("userid");
 		System.out.println("USER LOGOUT : "+userid);
 		session.removeAttribute("userid");
@@ -70,8 +42,8 @@ public class ServiceResource {
 	@GET
 	@Path("/all/")
 	@Produces("text/xml")
-	public UserHandler getAllUsers() {
-		return Server.uh;
+	public ServiceHandler getAllServices() {
+		return Server.serh;
 	}
 
 	
