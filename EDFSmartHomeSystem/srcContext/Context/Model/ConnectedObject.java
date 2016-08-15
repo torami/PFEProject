73064c0ce754ce50c766ac801f4f9e3eff127074
Rami <rami.torkhani@gmail.com>
2016-08-15 14:36:18 +0200

    create connectedobject beans

--- EDFSmartHomeSystem/srcContext/Context/Model/ConnectedObject.java
+++ EDFSmartHomeSystem/srcContext/Context/Model/ConnectedObject.java
@@ -1,5 +1,87 @@
 package Context.Model;
 
+
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+/**
+ * Beans Capteur this object represent a sensor like {TERMOSTAT}
+ *  @author J60277
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "connectedobject", propOrder = { "id","type","etat","emplacement"})
+@XmlRootElement(name = "connectedobject")
 public class ConnectedObject {
 
+	@XmlElement(name = "id")
+	private String id;
+	@XmlElement(name = "type")
+	private String type;
+	@XmlElement(name = "etat")
+	private String etat;
+	@XmlElement(name = "emplacement")
+	private String emplacement;
+	/**
+	 * this 's a default constructor for JAXB
+	 * @author J60277 
+	 */
+	public ConnectedObject() {	}
+	/**
+	 * This 's another constructor
+	 * @param type
+	 * 			Type of sensor
+	 * @param etat
+	 * 			State or value returned by the sensor
+	 * @param emplacement
+	 * 			the position of the sensor 
+	 * @author J60277
+	 */
+	public ConnectedObject( final String type,final String etat,final String emplacement) {
+		this.id = createConnectedObjectIdFormemplacement(emplacement,type);
+		this.emplacement = emplacement;
+		this.etat = etat;
+		this.type=type;
+	}
+	/**
+	 * Some Getters & Setters Tools
+	 * @author J60277
+	 */
+
+	public String getId() {
+		return id;
+	}
+	public String getType() {
+		return type;
+	}
+	public String getEtat() {
+		return etat;
+	}
+	public String getEmplacement() {
+		return emplacement;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public void setEtat(String etat) {
+		this.etat = etat;
+	}
+	public void setEmplacement(String emplacement) {
+		this.emplacement = emplacement;
+		this.id = createConnectedObjectIdFormemplacement(emplacement,this.type);
+	}
+	public static String createConnectedObjectIdFormemplacement(final String emplacement,String type) {
+		String str = "";
+		str = emplacement + "@" + type;
+		return str;
+	}
+
 }
+
--- EDFSmartHomeSystem/srcService/service/resources/ServiceResource.java
+++ EDFSmartHomeSystem/srcService/service/resources/ServiceResource.java
@@ -1,29 +1,61 @@
 package service.resources;
 
 
+
+import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
-import service.model.Service;
-import service.model.handlers.ServiceHandler;
-import service.server.Server;
-import service.server.TemplateEngine;
+import javax.ws.rs.core.MultivaluedMap;
 
+import Context.server.Server;
+import Context.server.TemplateEngine;
+import Context.Model.User;
+import Context.Model.Handler.UserHandler;
 
-@Path("/service")
+@Path("/user")
 public class ServiceResource {
 	@GET
-	@Path("/id/{serviceid}")
+	@Path("/id/{userid}")
 	@Produces("text/xml")
-	public Service getServiceFromId(@PathParam("serviceid") String serviceid) {
-		Service u = Server.serh.getServiceFromId(serviceid);
+	public User getUserFromId(@PathParam("userid") String userid) {
+		User u = Server.uh.getUserFromId(userid);
 		return u;
 	}
 
+	@POST
+	@Path("/create")
+	@Produces("text/html")
+	@Consumes("application/x-www-form-urlencoded")
+	public String create(@Context HttpServletRequest req, MultivaluedMap<String, String> formParams){
+		TemplateEngine.setSession(req.getSession());
+		String login = formParams.getFirst("login");
+		String password = formParams.getFirst("password");
+		Server.uh.createUser(login, password);
+		return TemplateEngine.goHome();
+	}	
+	
+	@POST
+	@Path("/login")
+	@Produces("text/html")
+	@Consumes("application/x-www-form-urlencoded")
+	public String login(@Context HttpServletRequest req, @FormParam("login") String login, MultivaluedMap<String, String> formParams) {
+		TemplateEngine.setSession(req.getSession());
+		String password = formParams.getFirst("password");
+		if(Server.uh.VerifyloginAndPassword(login, password)==true){
+			HttpSession session = req.getSession();
+			session.setAttribute("userid", User.createUserId(login));
+			System.out.println("USER LOGIN : "+login);
+		}
+		return TemplateEngine.goHome();
+	}	
 	
 	@GET
 	@Path("/logout")
@@ -31,19 +63,17 @@ public class ServiceResource {
 	public String logout(@Context HttpServletRequest req) {
 		TemplateEngine.setSession(req.getSession());
 		HttpSession session = req.getSession();
-		String serviceid = (String) session.getAttribute("serviceid");
-		System.out.println("Service Repo LOGOUT : "+serviceid);
-		session.removeAttribute("serviceid");
+		String userid = (String) session.getAttribute("userid");
+		System.out.println("USER LOGOUT : "+userid);
+		session.removeAttribute("userid");
 		return TemplateEngine.redirect("/", 2, "you will be disconnected in 2 seconds.");
 	}	
 	
 	@GET
 	@Path("/all/")
 	@Produces("text/xml")
-	public ServiceHandler getAllServices() {
-		return Server.serh;
+	public UserHandler getAllUsers() {
+		return Server.uh;
 	}
-
-	
 
 }
